052eba4bf90912f8205014b51356a14295118b5e
Keen1 <ke3n1@proton.me>
2024-03-08 19:00:35 -0500

    Changes to be committed: 	new file:   README.md 	new file:   Sniffer 	modified:   Sniffer.c
    Fixed bugs in Sniffer and learned how to link pcap library

---
+++ README.md
@@ -0,0 +1,4 @@
+Current requirement to compile Sniffer.c:
+	- link the pcap library w/ the following:
+		gcc Sniffer.c -o Sniffer -lpcap
+		
--- Sniffer.c
+++ Sniffer.c
@@ -7,9 +7,16 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
+
 pcap_t* handle;//pcap handle pointer
-int linkhdrlen;
-int packets;
+int linkhdrlen;// datalink layer header len
+int packets;//packet count for total captured
+
+//prototypes
+void stop_capture(int signum);
+pcap_t* create_pcap_handle(char* device, char* filter);
+void get_link_header_len(pcap_t* handle);
+void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr);
 
 
 int main(int argc, char *argv[]){
@@ -59,6 +66,11 @@ int main(int argc, char *argv[]){
 	}
 	
 	//start the packet capture with given or forever if 0
+	if(pcap_loop(handle, count, packet_handler, (u_char*)NULL) < 0){
+		fprintf(stderr, "pcap_loop failed: %s\n", pcap_geterr(handle));
+		return -1;
+	}
+	stop_capture(0);
 	
 }
 
@@ -86,7 +98,7 @@ pcap_t* create_pcap_handle(char* device, char* filter){
 	}
 	//get the network dev src ip and netmask
 	if(pcap_lookupnet(device, &scrip, &netmask, errbuf) == PCAP_ERROR){
-		fprintf(stderr, "pcap_lookupnet: &\n", errbuf);
+		fprintf(stderr, "pcap_lookupnet: %s\n", errbuf);
 		return NULL;
 	}
 	//open device for capture
@@ -124,8 +136,8 @@ void get_link_header_len(pcap_t* handle){
 		case DLT_NULL:
 			linkhdrlen = 4;
 			break;
-		case DL_EN10MB:
-			linkhdrlen = 14l
+		case DLT_EN10MB:
+			linkhdrlen = 14;
 			break;
 		case DLT_SLIP:
 		case DLT_PPP:
@@ -138,7 +150,7 @@ void get_link_header_len(pcap_t* handle){
 	}
 }
 //handle captured packets
-void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const uchar *packetptr){
+void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr){
 	
 	struct ip* iphdr;
 	struct icmp* icmphdr;
@@ -162,7 +174,7 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const uch
 	switch(iphdr ->ip_p){
 		case IPPROTO_TCP:
 			tcphdr = (struct tcphdr*)packetptr;
-			printf("TCP %s:%d -> %s:%d\n", scrip, ntohs(tcphdr->th_sport),
+			printf("TCP %s:%d -> %s:%d\n", srcip, ntohs(tcphdr->th_sport),
 					dstip, ntohs(tcphdr->th_dport));
 			printf("%s\n", iphdrInfo);
 			printf("%c%c%c%c%c%c Seq: 0x%x Ack: 0x%x Win 0x%x TcpLen: %d\n",
@@ -179,7 +191,7 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const uch
 			break;
 		case IPPROTO_UDP:
 			udphdr = (struct udphdr*)packetptr;
-			printf("UDP %s:%d -> %s:%d\n", scrip, ntohs(udphdr->uh_sport),
+			printf("UDP %s:%d -> %s:%d\n", srcip, ntohs(udphdr->uh_sport),
 					dstip, ntohs(udphdr->uh_dport));
 			printf("%s\n", iphdrInfo);
 			printf("++++++++++++++++++++\n\n");
@@ -187,7 +199,7 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const uch
 			break;
 		case IPPROTO_ICMP:
 			icmphdr = (struct icmp*)packetptr;
-			printf("ICMP %s -> %s\n", scrip, dstip);
+			printf("ICMP %s -> %s\n", srcip, dstip);
 			printf("%s\n", iphdrInfo);
 			printf("Type: %d Code:%d ID%d Seq%d\n", icmphdr->icmp_type, icmphdr->icmp_code,
 					ntohs(icmphdr->icmp_hun.ih_idseq.icd_id), ntohs(icmphdr->icmp_hun.ih_idseq.icd_seq));
@@ -202,12 +214,12 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const uch
 void stop_capture(int signo){
 	struct pcap_stat stats;
 	
-	if(pcap_stats(pd, &stats) > 0){
+	if(pcap_stats(handle, &stats) > 0){
 		printf("\n %d packets captured\n", packets);
 		printf("%d packets received\n", stats.ps_recv);
 		printf("%d packets dropped\n\n", stats.ps_drop);
 	}
-	pcap_close(pd);
+	pcap_close(handle);
 	exit(0);
 }
 
